47ed755e9fa30a4095135be991c6a104e91772ae
root <root@CHRIS.localdomain>
2022-11-06 04:10:50 +0200

    update

--- app/core/models.py
+++ app/core/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=255)
+    username   = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
     last_name  = models.CharField(max_length=255)
-    email      = models.EmailField()
-    password   = models.IntegerField()
+    avatar     = models.FileField(upload_to='media/images',max_length=255,null=True)
+    email      = models.EmailField(max_length=100)
+    password   = models.CharField(max_length=32)
     is_staff   = models.BooleanField()
     is_active  = models.BooleanField()
     
     def __str__(self):
         return self.username
+    
--- app/core/urls.py
+++ app/core/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path,include
-from core.views import UserViewset
+from django.urls import path, include
+from .views import UserViewset
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('user',UserViewset)
+
+urlpatterns = router.urls
